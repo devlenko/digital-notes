74200d40983a53070910fad49e682c93b38333c1
devlenko <279883133+devlenko@users.noreply.github.com>
2026-09-07 20:11:23 -0400

    Prevent notes from being dragged outside window

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const note = document.querySelector(".note");
 const dragBar = note.querySelector(".drag-bar");
 
 let isDragging = false;
+ 
+let canMoveOnX = true, canMoveOnY = true;
+let xUnlockPos = null, yUnlockPos = null;
 
 dragBar.addEventListener("pointerdown", event => {
     // Force the bar to keep tracking the event even if it moves outside the bar's bounds
@@ -12,8 +15,50 @@ dragBar.addEventListener("pointerdown", event => {
 dragBar.addEventListener("pointermove", event => {
     if (!isDragging) return;
 
-    note.style.left = `${note.offsetLeft + event.movementX}px`;
-    note.style.top = `${note.offsetTop + event.movementY}px`;
+    let proposedLeft = note.offsetLeft + event.movementX;
+    let proposedTop = note.offsetTop + event.movementY;
+
+    if (canMoveOnX) {
+        if (proposedLeft < 0) {
+            canMoveOnX = false;
+            xUnlockPos = event.clientX;
+            proposedLeft = 0;
+        }
+        else if (proposedLeft > window.innerWidth - note.getBoundingClientRect().width) {
+            canMoveOnX = false;
+            xUnlockPos = event.clientX;
+            proposedLeft = window.innerWidth - note.getBoundingClientRect().width;
+        }
+
+        note.style.left = `${proposedLeft}px`;
+    }
+    else if (
+        (xUnlockPos < window.innerWidth / 2 && event.clientX >= xUnlockPos) ||
+        (xUnlockPos > window.innerWidth / 2 && event.clientX <= xUnlockPos)
+    ) {
+        canMoveOnX = true;
+    }
+
+    if (canMoveOnY) {
+        if (proposedTop < 0) {
+            canMoveOnY = false;
+            yUnlockPos = event.clientY;
+            proposedTop = 0;
+        }
+        else if (proposedTop > window.innerHeight - note.getBoundingClientRect().height) {
+            canMoveOnY = false;
+            yUnlockPos = event.clientY;
+            proposedTop = window.innerHeight - note.getBoundingClientRect().height;
+        }
+
+        note.style.top = `${proposedTop}px`;
+    }
+    else if (
+        (yUnlockPos < window.innerHeight / 2 && event.clientY >= yUnlockPos) ||
+        (yUnlockPos > window.innerHeight / 2 && event.clientY <= yUnlockPos)
+    ) {
+        canMoveOnY = true;
+    }
 })
 
 function stopDragging(event) {
