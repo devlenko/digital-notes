dc90fa90c084661fc96b60dfb20e448f946ef733
devlenko <279883133+devlenko@users.noreply.github.com>
2026-09-12 21:57:19 -0400

    Add note sticking to borders when window resized

--- script.js
+++ script.js
@@ -4,24 +4,25 @@ const noteText = note.querySelector("textarea");
 
 let isDragging = false;
 
-let startVals = { x: null, y: null, left: null, top: null };
+let dragStartVals = { x: null, y: null, left: null, top: null };
+let resizeStartVals = { width: noteText.getBoundingClientRect().width, height: noteText.getBoundingClientRect().height };
 
 dragBar.addEventListener("pointerdown", event => {
     // Force the bar to keep tracking the event even if it moves outside the bar's bounds
     dragBar.setPointerCapture(event.pointerId);
     isDragging = true;
 
-    startVals = { x: event.clientX, y: event.clientY, left: note.offsetLeft, top: note.offsetTop};
+    dragStartVals = { x: event.clientX, y: event.clientY, left: note.offsetLeft, top: note.offsetTop};
 });
 
 dragBar.addEventListener("pointermove", event => {
     if (!isDragging) return;
 
-    const deltaX = event.clientX - startVals.x;
-    const deltaY = event.clientY - startVals.y;
+    const deltaX = event.clientX - dragStartVals.x;
+    const deltaY = event.clientY - dragStartVals.y;
 
-    const newLeft = Math.max(0, Math.min(startVals.left + deltaX, window.innerWidth - note.offsetWidth));
-    const newTop = Math.max(0, Math.min(startVals.top + deltaY, window.innerHeight - note.offsetHeight));
+    const newLeft = Math.max(0, Math.min(dragStartVals.left + deltaX, window.innerWidth - note.offsetWidth));
+    const newTop = Math.max(0, Math.min(dragStartVals.top + deltaY, window.innerHeight - note.offsetHeight));
 
     // Update position on screen
     note.style.left = `${newLeft}px`;
@@ -35,6 +36,10 @@ dragBar.addEventListener("pointermove", event => {
 function stopDragging(event) {
     dragBar.releasePointerCapture(event.pointerId);
     isDragging = false;
+
+    // Store original width and height in case the window is resized
+    resizeStartVals.width = noteText.getBoundingClientRect().width;
+    resizeStartVals.height = noteText.getBoundingClientRect().height;
 }
 
 dragBar.addEventListener("pointerup", stopDragging);
@@ -49,7 +54,30 @@ const textareaSizeSyncer = new ResizeObserver(entries => {
 
         if (inlineStyle.width !== computedStyle.width)  inlineStyle.width = computedStyle.width;
         if (inlineStyle.height !== computedStyle.height) inlineStyle.height = computedStyle.height;
+
+        // Store new width and height if the textarea is not actively being resized by the window
+        // This prevents the note from continuing to shrink while the window is resized
+        if (noteText.getBoundingClientRect().right < window.innerWidth) resizeStartVals.width = noteText.getBoundingClientRect().width;
+        if (noteText.getBoundingClientRect().bottom < window.innerHeight) resizeStartVals.height = noteText.getBoundingClientRect().height;
     }
 });
 
 textareaSizeSyncer.observe(noteText);
+
+window.addEventListener("resize", event => {
+    if (note.getBoundingClientRect().right >= window.innerWidth) {
+        const newLeft = Math.max(0, window.innerWidth - resizeStartVals.width);
+
+        note.style.left = `${newLeft}px`;
+        noteText.style.setProperty("--left-pos", `${newLeft}px`);
+        noteText.style.width = `${resizeStartVals.width}px`;
+    }
+
+    if (note.getBoundingClientRect().bottom >= window.innerHeight) {
+        const newTop = Math.max(0, window.innerHeight - resizeStartVals.height - dragBar.getBoundingClientRect().height);
+
+        note.style.top = `${newTop}px`;
+        noteText.style.setProperty("--top-pos", `${newTop}px`);
+        noteText.style.height = `${resizeStartVals.height}px`;
+    }
+});
